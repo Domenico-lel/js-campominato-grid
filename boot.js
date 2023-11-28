5a61c2eea076e57b599b1e7ebad5a2468c5eb921
Domenico-lel <domenicolella2@gmail.com>
2023-11-28 19:14:41 +0100

    aggiunto tasto con relativo js

--- boot.js
+++ boot.js
@@ -4,16 +4,17 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
 
-// sezione dell' elemento contenitore
+// sezione dell' elemento contenitore e pulsante
 const gridElement = document.getElementById("grid");
+const startButton = document.getElementById("startButton");
 
 for (let i = 1; i <= 64; i++) {
     const newElement = createMyElement("div", "square");
-    
+
     // Aggiungi il numero come testo all'interno del div
     newElement.textContent = i;
 
-    newElement.addEventListener("click", 
+    newElement.addEventListener("click",
         function () {
             this.classList.add("clicked");
         }
@@ -21,6 +22,13 @@ for (let i = 1; i <= 64; i++) {
 
     gridElement.append(newElement);
 }
+
+startButton.addEventListener("click",
+    function () {
+    // Aggiungi qui la logica di inizio del gioco
+    alert("Il gioco è iniziato!");
+    }
+);
 
 // definizione funzione 
 function createMyElement(tagtype, classname) {
